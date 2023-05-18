019e964f8292ac69fd1b28784aef6f30e63f5b2e
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2023-05-18 22:07:11 +0330

    add redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import OrderOrRules from "@/components/OrderOrRules";
 import TopPart from "@/components/TopPart";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
+import ButtomCart from "@/components/ButtomCart";
 
 export default function Home() {
   return (
-    <main className="scroll-smooth ">
-    
-      <Navbar />
-      <div className="bg-gray-50">
-        <TopPart />
-        <OrderOrRules />
-      </div>
-    </main>
+    <Provider store={store}>
+      <main className="min-h-screen">
+        <Navbar />
+        <div className="bg-gray-50">
+          <TopPart />
+          <OrderOrRules />
+        </div>
+        <ButtomCart />
+      </main>
+    </Provider>
   );
 }
