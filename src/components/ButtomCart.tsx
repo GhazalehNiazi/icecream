934cdab2e956c3e8efebd69e39ea463e30d2f5d0
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2023-05-18 22:58:14 +0330

    feat/make info dynamic

--- src/components/ButtomCart.tsx
+++ src/components/ButtomCart.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 
 function ButtomCart() {
+  interface item {
+    name: string;
+    id: string;
+    description: string;
+    price: number;
+    img: string;
+    quntity: number;
+  }
+
   const items = useSelector((state) => state.cart.items);
   const total = useSelector((state) => state.cart.total);
-  console.log(items);
+
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
+  useEffect(() => {
+    const initialValue = 0;
+    if (items.length > 0) {
+      const num = items.reduce(
+        ( currentvalue: number , item: item,) => currentvalue + item.quntity,
+        initialValue
+      );
+      setNumberOfItems(num);
+    }
+  }, [items]);
 
   return (
     items.length && (
-      <div className="text-slate-50 px-4 py-2 bottom-0 sticky bg-cyan-500 h-12 w-full flex justify-between w-full flex-row-reverse">
+      <div className="text-slate-50 px-4 py-2 bottom-0 sticky bg-cyan-500 h-14 w-full flex justify-between w-full flex-row-reverse items-center">
         <div className="text-xl flex flex-row-reverse items-center gap-1">
           <ShoppingCartIcon width={20} height={20} />
           سبد خرید
-          <span className="text-base">({items.length})</span>
+          <span className="text-base">({numberOfItems})</span>
         </div>
-        <div className="text-sm py-2 px-4 bg-cyan-400 rounded-2xl flex items-center flex-row-reverse gap-2">
+        <div className="text-sm h-8 py-1 px-4 bg-cyan-400 rounded-2xl flex items-center flex-row-reverse gap-2 ">
           <span>{total}</span>
           <span> تومان</span>
         </div>
