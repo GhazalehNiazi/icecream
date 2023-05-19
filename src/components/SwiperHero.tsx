020a7f1e00bc33be16ad148ac4e627438947e507
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2023-05-19 13:53:28 +0330

    FIX: image size

--- src/components/SwiperHero.tsx
+++ src/components/SwiperHero.tsx
@@ -10,75 +10,77 @@ import "swiper/css/pagination";
 
 function SwiperHero() {
   return (
-    <Swiper
-      cssMode={true}
-      pagination={{
-        clickable: true,
-      }}
-      mousewheel={true}
-      loop={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      modules={[Pagination, Mousewheel, Autoplay]}
-      className="max-h-[8rem] max-w-full "
-    >
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={800}
-          width={400}
-          alt="slide1"
-          src="/slide1.jpg"
-        />
-      </SwiperSlide>
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={700}
-          width={400}
-          alt="slide2"
-          src="/slide2.jpg"
-        />
-      </SwiperSlide>{" "}
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={700}
-          width={400}
-          alt="slide3"
-          src="/slide3.jpg"
-        />
-      </SwiperSlide>{" "}
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={700}
-          width={400}
-          alt="slide4"
-          src="/slide4.jpg"
-        />
-      </SwiperSlide>{" "}
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={700}
-          width={400}
-          alt="slide5"
-          src="/slide5.jpg"
-        />
-      </SwiperSlide>{" "}
-      <SwiperSlide className="">
-        <Image
-          className=""
-          height={700}
-          width={400}
-          alt="slide6"
-          src="/slide6.jpg"
-        />
-      </SwiperSlide>
-    </Swiper>
+    <div className="lg:w-9/12 ">
+      <Swiper
+        cssMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        mousewheel={true}
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        modules={[Pagination, Mousewheel, Autoplay]}
+        className="lg:max-h-[19rem] max-w-full m-auto"
+      >
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide1"
+            src="/slide1.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide2"
+            src="/slide2.jpg"
+          />
+        </SwiperSlide>{" "}
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide3"
+            src="/slide3.jpg"
+          />
+        </SwiperSlide>{" "}
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide4"
+            src="/slide4.jpg"
+          />
+        </SwiperSlide>{" "}
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide5"
+            src="/slide5.jpg"
+          />
+        </SwiperSlide>{" "}
+        <SwiperSlide className="">
+          <Image
+            className=""
+            height={900}
+            width={900}
+            alt="slide6"
+            src="/slide6.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
 
