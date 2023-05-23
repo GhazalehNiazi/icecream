16e7668b197c4e1d2d5e29aebb20ad447e4fdcd8
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2023-05-23 18:20:21 +0330

    change cursor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className="bg-white px-4 flex full-width justify-between pt-3 pb-2 font-extralight border-b border-gray-200">
       <Image alt="logo" width={60} height={60} src="/logo.png" />
       <div className="flex gap-2 items-center">
-        <Bars3BottomLeftIcon width={25} />
+        <Bars3BottomLeftIcon width={25} className="cursor-pointer" />
         <button className=" font-extralight px-2 py-2 h-min border border-sky-400 rounded-md flex items-center gap-2">
           ورود / عضویت
           <UserIcon width={25} />
