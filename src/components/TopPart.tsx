3cad6c5527a3f328216b1d795b19c0274fee4b65
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2023-05-23 22:22:01 +0330

    fix

--- src/components/TopPart.tsx
+++ src/components/TopPart.tsx
@@ -15,13 +15,13 @@ function TopPart() {
           <div className="bg-white  border rounded-md px-5 py-2 border-gray-200 flex flex-col items-center justify-center gap-1 md:items-start md:px-7 md:py-2 md:w-max">
             <div className="font-bold">میانگین زمان تحویل</div>
             <div className="flex justify-between w-full text-sm md:flex-col ">
-              <div className="font-extralight md:flex md:gap-7  ">
-                <span className="md:basis-1/2">حضوری</span>{" "}
+              <div className="font-extralight md:flex md:gap-9  ">
+                <span className="md:basis-1/3">حضوری</span>{" "}
                 <span className="md:basis-1/2 md:break-keep">تا 15 دقیقه </span>
               </div>
               <div className="font-extralight flex gap-0.5  md:gap-4">
                 <span className="">ارسال با پیک</span>
-                <div className="flex gap-0.5 ">
+                <div className="flex  ">
                   <ExclamationTriangleIcon
                     width={15}
                     className="text-orange-500"
